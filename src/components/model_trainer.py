731978f4d1f814c1b6200d272d8453ce5e91fdbd
Vikas Sharma <vikassharma6990@outlook.com>
2025-08-25 23:31:26 +0530

    Best Model Find

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -57,9 +57,9 @@ class ModelTrainer:
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best model found on both training and testing dataset")
-            #preprocessing_obj = save_object(file_path=self.model_trainer_config.trained_model_file_path,
-            #                               obj=best_model
-            #                               )
+            preprocessing_obj = save_object(file_path=self.model_trainer_config.trained_model_file_path,
+                                           obj=best_model
+                                           )
             predicted = best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
             return r2_square
