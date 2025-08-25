297a7878bcfa8623cd059f54260a044f5c724623
Vikas Sharma <vikassharma6990@outlook.com>
2025-08-25 23:09:28 +0530

    Best Model Find

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 import numpy as np
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     """
@@ -18,5 +19,26 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
         logging.info(f"Object saved at {file_path}")
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Function to evaluate multiple machine learning models and return their R2 scores.
+    """
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred) #here r2_score is used as the metric for evaluation which is used to check the accuracy of the model and the value of r2_score ranges from 0 to 1
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
     except Exception as e:
         raise CustomException(e, sys)
